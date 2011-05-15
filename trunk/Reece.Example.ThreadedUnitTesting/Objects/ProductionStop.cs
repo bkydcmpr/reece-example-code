259af5cb036439e8179823fe779b689b0ce6dc2d
me@stephenreece.com <me@stephenreece.com@edb9b146-9d3c-d4d1-ba45-d3f2e50efe54>
2011-05-15 00:41:38 +0000

    Inital post

---
+++ trunk/Reece.Example.ThreadedUnitTesting/Objects/ProductionStop.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Reece.Example.ThreadedUnitTesting.Objects
+{
+    public class ProductionStop
+    {
+        public object StateData { get; set; }
+        public Guid NextNode { get; set; }
+    }
+}
