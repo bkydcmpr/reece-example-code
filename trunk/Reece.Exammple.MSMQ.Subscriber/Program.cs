db1f19d179862644cb03a1bf383fc9941a311f85
me@stephenreece.com <me@stephenreece.com@edb9b146-9d3c-d4d1-ba45-d3f2e50efe54>
2011-06-02 04:09:16 +0000

    Added example for MSMQ.

---
+++ trunk/Reece.Exammple.MSMQ.Subscriber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Messaging;
+using System.Threading;
+
+namespace Reece.Exammple.MSMQ.Subscriber
+{
+    class Program
+    {
+        const string QueuePath = @".\Private$\ExampleQueue";
+
+        private static volatile bool _running;
+        private static MessageQueue _queue;
+
+        static void Main(string[] args)
+        {
+            _running = true;
+
+            _queue = new MessageQueue(QueuePath);
+
+            ThreadPool.QueueUserWorkItem((state) =>
+                                             {
+                                                 while (_running)
+                                                 {
+                                                     try
+                                                     {
+                                                         var message = _queue.Receive(new TimeSpan(0, 0, 0, 1));
+                                                         message.Formatter = new XmlMessageFormatter(new String[] {"System.String,mscorlib"});
+                                                         Console.WriteLine(message.Body.ToString());
+                                                     }
+                                                     catch (Exception)
+                                                     {
+                                                     }
+                                                 }
+                                             }
+                );
+
+            Console.WriteLine("Press a key to exit.");
+            Console.ReadKey();
+            _running = false;
+        }
+    }
+}
