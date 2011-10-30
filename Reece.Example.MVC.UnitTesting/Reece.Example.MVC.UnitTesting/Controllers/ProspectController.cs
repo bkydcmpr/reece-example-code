d695c7f15c672ad82673b0f94a7a61b4c7e86fd8
me <me@stephenreece.com>
2011-10-30 01:44:25 +0000

    First post for MVC unit testing article.

---
+++ Reece.Example.MVC.UnitTesting/Reece.Example.MVC.UnitTesting/Controllers/ProspectController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Reece.Example.MVC.UnitTesting.Biz.Interfaces;
+using Reece.Example.MVC.UnitTesting.Models;
+
+namespace Reece.Example.MVC.UnitTesting.Controllers
+{
+    public class ProspectController : Controller
+    {
+        public IProspectRequest ProspectRequest { get; set; }
+
+        public ActionResult New()
+        {
+            return View(new Prospect());
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult New( Prospect prospect )
+        {
+            Guid id = ProspectRequest.AddProspect(prospect.Name, prospect.Phone);
+            Prospect result = new Prospect()
+                                  {
+                                      ID = id,
+                                      Name = prospect.Name,
+                                      Phone = prospect.Phone
+                                  };
+            return Json(result);
+        }
+
+    }
+}
---
+++ Reece.Example.MVC.UnitTesting/Reece.Example.MVC.UnitTesting/Models/Prospect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Reece.Example.MVC.UnitTesting.Models
+{
+    public class Prospect
+    {
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Phone")]
+        public string Phone { get; set; }
+
+        public Guid ID { get; set; }
+    }
+}
